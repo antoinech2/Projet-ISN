cebc23d1436b6acdcdc07afca41a49cdda396966
antoinech2 <smb.antoine@gmail.com>
2020-03-07 12:47:15 +0100

    Génération fonctionnelle Le programme génère des maps aléatoires jusqu'à ce que le chemin fasse au moins 30 cases de long. Directions renommées avec des constantes L'arrivée est forcément sur un bord (la génération s'arrête lorsque le chemin atteint un bord).

--- map_generator.py
+++ map_generator.py
@@ -9,76 +9,86 @@ map_size = (10,10)
 force_space_between_path = True
 path_coords = []
 
+#Constantes de direction:
+DIR_NORTH = 1
+DIR_SOUTH = 2
+DIR_EAST = 3
+DIR_WEST = 4
+
 #total_possibilities = (map_size[0]-2)*2 + (map_size[1]-2)*2
 #chosen_case_id = random.randint(0,total_possibilities)
 
-if random.randint(0,1) == 1:
-	if random.randint(0,1) == 1:
+if random.randint(0,1) == 1:       		#X=1 ou X=taille
+	if random.randint(0,1) == 1:		#X=1
 		x_start = 1
+		direction_start = DIR_SOUTH
 	else:
-		x_start = map_size[0]
+		x_start = map_size[0]			#X=taille
+		direction_start = DIR_NORTH
 	y_start = random.randint(2,map_size[1]-1)
-else:
+else:									#Y=1 ou Y=taille
 	if random.randint(0,1) == 1:
-		y_start = 1
+		y_start = 1						#Y=1
+		direction_start = DIR_EAST
 	else:
-		y_start = map_size[1]
+		y_start = map_size[1]			#Y=taille
+		direction_start = DIR_WEST
 	x_start = random.randint(2,map_size[0]-1)
 
 start_coords = (x_start,y_start)
-path_coords.append(start_coords)
-number_voisins = 10
-no_solution = False
-print(x_start,y_start)
+number_calculation = 0
 
-while no_solution == False:
-	direction = random.randint(1,4)
-	number_change_direction = 0
-	while not ((number_voisins < 2) and ((current_case[0],current_case[1]) not in path_coords)):
-		#print("Début loop:", current_case, last_case)
-		current_case = list(path_coords[-1])
-		if direction == 1:
-			#Direction: Nord
-			if current_case[0] > 2:
+while len(path_coords) < 30:
+	number_calculation += 1
+	path_coords = []
+	path_coords.append(start_coords)
+	number_voisins = 2
+	no_solution = False
+	direction = direction_start
+
+	while no_solution == False:
+		number_change_direction = 0
+		while not ((number_voisins < 2) and ((current_case[0],current_case[1]) not in path_coords)):
+			#print("Début loop:", current_case, last_case)
+			current_case = list(path_coords[-1])
+			if direction == DIR_NORTH:
+				#Direction: Nord
 				current_case[0] -= 1
-		elif direction == 2:
-			#Direction: Sud
-			if current_case[0] < map_size[0]-1:
+			elif direction == DIR_SOUTH:
+				#Direction: Sud
 				current_case[0] += 1
-		elif direction == 3:
-			#Direction: Est
-			if current_case[1] < map_size[1]-1:
+			elif direction == DIR_EAST:
+				#Direction: Est
 				current_case[1] += 1
-		elif direction == 4:
-			#Direction: Ouest
-			if current_case[1] > 2:
+			elif direction == DIR_WEST:
+				#Direction: Ouest
 				current_case[1] -= 1
 
-		number_voisins = 0
-		if force_space_between_path:
-			if (current_case[0]+1,current_case[1]) in path_coords:
-				number_voisins +=1
-			if (current_case[0]-1,current_case[1]) in path_coords:
-				number_voisins +=1
-			if (current_case[0],current_case[1]+1) in path_coords:
-				number_voisins +=1
-			if (current_case[0],current_case[1]-1) in path_coords:
-				number_voisins +=1
+			number_voisins = 0
+			if force_space_between_path:
+				if (current_case[0]+1,current_case[1]) in path_coords:
+					number_voisins +=1
+				if (current_case[0]-1,current_case[1]) in path_coords:
+					number_voisins +=1
+				if (current_case[0],current_case[1]+1) in path_coords:
+					number_voisins +=1
+				if (current_case[0],current_case[1]-1) in path_coords:
+					number_voisins +=1
 
-		print("Echec:", direction, number_change_direction)
-
-		if number_change_direction == 4:
-			no_solution = True
-			break
-		else:
-			number_change_direction += 1
-			if direction == 4:
-				direction = 1
+			if number_change_direction == 4:
+				no_solution = True
+				break
 			else:
-				direction += 1
-	path_coords.append((current_case[0],current_case[1]))
-	print("Succès:",current_case)
-	#last_case = current_case
+				number_change_direction += 1
+				if direction == 4:
+					direction = 1
+				else:
+					direction += 1
+			direction = random.randint(1,4)
+		if (current_case[0] == 1) or (current_case[0] == map_size[0]) or (current_case[1] == 1) or (current_case[1] == map_size[1]):
+			no_solution = True
+
+		path_coords.append((current_case[0],current_case[1]))
 
 print(path_coords)
 
@@ -93,3 +103,5 @@ for row in range (1,map_size[0]+1):
 		else:
 			print("0", end="")
 	print("")
+
+print("Génération terminée en",number_calculation,"tentatives")
