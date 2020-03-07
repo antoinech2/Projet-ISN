6d992b97136735eb42e9fa51ecb5888127a37d83
antoinech2 <smb.antoine@gmail.com>
2020-03-07 13:35:49 +0100

    Améliorations Calcul du point de départ dans une fonction Tests de performances

--- map_generator.py
+++ map_generator.py
@@ -15,38 +15,42 @@ DIR_SOUTH = 2
 DIR_EAST = 3
 DIR_WEST = 4
 
-#total_possibilities = (map_size[0]-2)*2 + (map_size[1]-2)*2
-#chosen_case_id = random.randint(0,total_possibilities)
+def CalculateNewStartPosition():
+	if random.randint(0,1) == 1:       		#X=1 ou X=taille
+		if random.randint(0,1) == 1:		#X=1
+			x_start = 1
+			direction_start = DIR_SOUTH
+		else:
+			x_start = map_size[0]			#X=taille
+			direction_start = DIR_NORTH
+		y_start = random.randint(2,map_size[1]-1)
+	else:									#Y=1 ou Y=taille
+		if random.randint(0,1) == 1:
+			y_start = 1						#Y=1
+			direction_start = DIR_EAST
+		else:
+			y_start = map_size[1]			#Y=taille
+			direction_start = DIR_WEST
+		x_start = random.randint(2,map_size[0]-1)
 
-if random.randint(0,1) == 1:       		#X=1 ou X=taille
-	if random.randint(0,1) == 1:		#X=1
-		x_start = 1
-		direction_start = DIR_SOUTH
-	else:
-		x_start = map_size[0]			#X=taille
-		direction_start = DIR_NORTH
-	y_start = random.randint(2,map_size[1]-1)
-else:									#Y=1 ou Y=taille
-	if random.randint(0,1) == 1:
-		y_start = 1						#Y=1
-		direction_start = DIR_EAST
-	else:
-		y_start = map_size[1]			#Y=taille
-		direction_start = DIR_WEST
-	x_start = random.randint(2,map_size[0]-1)
+	start_coords = (x_start,y_start)
+	return start_coords, direction_start
 
-start_coords = (x_start,y_start)
 number_calculation = 0
 
 while len(path_coords) < 30:
+	if number_calculation%10000 == 0:
+		print("Tentatives:",number_calculation)
+
+	start_coords, direction_start = CalculateNewStartPosition()
 	number_calculation += 1
 	path_coords = []
 	path_coords.append(start_coords)
 	number_voisins = 2
-	no_solution = False
+	stop_generation = False
 	direction = direction_start
 
-	while no_solution == False:
+	while stop_generation == False:
 		number_change_direction = 0
 		while not ((number_voisins < 2) and ((current_case[0],current_case[1]) not in path_coords)):
 			#print("Début loop:", current_case, last_case)
@@ -76,7 +80,8 @@ while len(path_coords) < 30:
 					number_voisins +=1
 
 			if number_change_direction == 4:
-				no_solution = True
+				path_coords = []
+				stop_generation = True
 				break
 			else:
 				number_change_direction += 1
@@ -86,7 +91,7 @@ while len(path_coords) < 30:
 					direction += 1
 			direction = random.randint(1,4)
 		if (current_case[0] == 1) or (current_case[0] == map_size[0]) or (current_case[1] == 1) or (current_case[1] == map_size[1]):
-			no_solution = True
+			stop_generation = True
 
 		path_coords.append((current_case[0],current_case[1]))
 
@@ -99,9 +104,10 @@ for row in range (1,map_size[0]+1):
 		elif path_coords[-1] == (column,row):
 			print("A", end="")
 		elif (column,row) in path_coords:
-			print("-", end="")
+			print("°", end="")
 		else:
-			print("0", end="")
+			print(".", end="")
 	print("")
 
 print("Génération terminée en",number_calculation,"tentatives")
+print("Nombre de points:",len(path_coords))
