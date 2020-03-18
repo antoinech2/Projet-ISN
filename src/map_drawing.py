866a299ef07864593e45709ae3c0ee174c980a1a
Titouz41 <61840962+Titouz41@users.noreply.github.com>
2020-03-18 11:13:26 +0100

    Amélioration des graphismes Nouvelles images insanes fait sur paint 3D par le boss (va falloir changer ça ...). Mise en place d'un alléatoire à améliorer.

--- src/map_drawing.py
+++ src/map_drawing.py
@@ -18,11 +18,15 @@
 # Importation des modules:
 import pygame
 import math
+import random
 ############################################
 # Importation des images :
 
 img_chemin = pygame.image.load('../res/textures/map/chemin.png')
 img_herbe = pygame.image.load('../res/textures/map/herbe.png')
+img_herbev2 = pygame.image.load('../res/textures/map/herbev2.png')
+img_plante = pygame.image.load('../res/textures/map/plante.png')
+img_arbre = pygame.image.load('../res/textures/map/arbre.png')
 ############################################
 # Définition des fonctions locales:
 def CreateMapSurface(map_size,path_coords, screen_size):
@@ -30,16 +34,19 @@ def CreateMapSurface(map_size,path_coords, screen_size):
 	# Définition des constantes de fonction:
 	map_size_pixel = (0.8*screen_size[0],0.8*screen_size[1])
 	box_size_pixel = (math.floor(map_size_pixel[0]/map_size[0]),math.floor(map_size_pixel[1]/map_size[1]))
-	img_che = pygame.transform.scale(img_herbe,(box_size_pixel[0],box_size_pixel[1]))
-	img_herb = pygame.transform.scale(img_chemin,(box_size_pixel[0],box_size_pixel[1]))
+	img_che = pygame.transform.scale(img_chemin,(box_size_pixel[0],box_size_pixel[1]))
+	img_herb = pygame.transform.scale(img_herbe,(box_size_pixel[0],box_size_pixel[1]))
+	img_herbv2 = pygame.transform.scale(img_herbev2,(box_size_pixel[0],box_size_pixel[1]))
+	img_plant = pygame.transform.scale(img_plante,(box_size_pixel[0],box_size_pixel[1]))
+	img_arbr = pygame.transform.scale(img_arbre,(box_size_pixel[0],box_size_pixel[1]))
 	############################################
 	map_surface = pygame.Surface(map_size_pixel)
 	for column in range (1,map_size[0]+1):
 		for row in range (1,map_size[1]+1):
 			current_box = pygame.Surface(box_size_pixel)
 			if (column,row) in path_coords:
-				image = img_herb
-			else :
 				image = img_che
+			else :
+				image = image = random.choice([img_herbv2,img_plant,img_arbr])
 			map_surface.blit(image,((column-1)*box_size_pixel[0],(row-1)*box_size_pixel[1]))
 	return map_surface
